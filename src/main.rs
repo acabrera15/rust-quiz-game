c9ff7a37370cb6c53c686cfbb39def1d54d4a7a1
Andrew Cabrera <andrewcabrera@Mac.attlocal.net>
2025-01-26 17:58:36 -0800

    feature(main): hooked up the add function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rusqlite::{params, Connection, Result};
+use std::io;
 
 fn init_db() -> Result<()> {
     let path = "quiz_db.db3";
@@ -53,16 +54,63 @@ fn add_question(conn: &Connection, question: &str, options: Vec<(&str, bool)>) -
 fn main() -> Result<()> {
     init_db()?;
     let path = "quiz_db.db3";
-    let conn = Connection::open(path);
+    let conn = Connection::open(path)?;
 
     println!("Welcome to admin side");
     println!("Here you can manipulate the questions to be shown to users");
-    println!("Select one of the options below");
-    println!("-------------------------------");
-    println!("1. List questions");
-    println!("2. Add question");
-    println!("3. Update question");
-    println!("4. Delete question");
+
+    loop {
+        println!("Select one of the options below");
+        println!("-------------------------------");
+        println!("1. List questions");
+        println!("2. Add question");
+        println!("3. Update question");
+        println!("4. Delete question");
+
+        let mut input = String::new();
+        let mut num_input: u8;
+
+        loop {
+            input.clear();
+
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Unable to read line");
+
+            num_input = match input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("enter a valid number \n");
+                    continue;
+                }
+            };
+
+            if num_input > 4 || num_input < 1 {
+                println!("Enter a number from 1-4 \n");
+                continue;
+            }
+
+            break;
+        }
+
+        match num_input {
+            1 => println!("list \n"),
+            2 => {
+                if let Err(err) = add_question(
+                    &conn,
+                    "How tall is Andrew?",
+                    vec![("5'8", true), ("5'2", false), ("5'0", false)],
+                ) {
+                    println!("Failed to add question: {}", err);
+                } else {
+                    println!("Question added successfully");
+                }
+            }
+            3 => println!("update \n"),
+            4 => println!("delete \n"),
+            _ => println!("invlad option \n"),
+        }
+    }
 
     Ok(())
 }
