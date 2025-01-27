f414c62e786b669d8b2689338c223dba07ac23a4
Andrew Cabrera <andrewcabrera@Mac.attlocal.net>
2025-01-26 16:15:05 -0800

    feature(db): added db connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+
+fn init_db() -> Result<()> {
+    let path = "quiz_db.db3";
+
+    let conn = Connection::open(path)?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY,
+            question TEXT NOT NULL
+            )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS options (
+            id INTEGER PRIMARY KEY,
+            question_id INTEGER NOT NULL,
+            option_text TEXT NOT NULL,
+            is_correct BOOLEAN NOT NULL,
+            FOREIGN KEY(question_id) REFERENCES questions(id)
+        )",
+        [],
+    )?;
+
+    Ok(())
+}
+
+// TODO: add question
+
+// TODO: delete question
+
+// TODO: update question
+
+// TODO: list questions
+
+fn main() -> Result<()> {
+    init_db()?;
+    Ok(())
 }
